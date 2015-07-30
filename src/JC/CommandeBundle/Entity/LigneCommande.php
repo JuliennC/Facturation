f017816bd7c07b60ff9db7590d544c145b68ea8e
JuliennC <julienchaudoin@hotmail.fr>
2015-07-30 08:35:44 +0200

    Correction 2

--- src/JC/CommandeBundle/Entity/LigneCommande.php
+++ src/JC/CommandeBundle/Entity/LigneCommande.php
@@ -34,7 +34,7 @@ class LigneCommande
     /**
      * @var string
      *
-     * @ORM\Column(name="Reference", type="string", length=255)
+     * @ORM\Column(name="Reference", type="string", length=255, nullable=true)
      */
     private $reference;
 
